bc0c2e1e0583f77b0b2d747bed928f733e2b28c2
Farooq haider alvi <farooqhaideralvi1999@gmail.com>
2023-12-07 10:49:13 +0500

    added two deletion logics in bst

--- BinarySearchTree/BinarySearchTree/BinarySearchTree.cpp
+++ BinarySearchTree/BinarySearchTree/BinarySearchTree.cpp
@@ -62,7 +62,7 @@ public:
 		}
 	}
 
-	bstNode<t>*& search(t val) {
+	bstNode<t>* search(t val) {
 
 		bstNode<t>* temp = root;
 		while (temp) {
@@ -84,20 +84,21 @@ public:
 		if (!temp)
 			return;
 		
-		inOrderTraversal(temp->left);
+
 		cout << temp->data << endl;
+		inOrderTraversal(temp->left);
 		inOrderTraversal(temp->right);
 
 	}
 
+	//these three function contribute to delete node from book
 	void remove(int num)
 		 {
 
 		 deleteNode(num, root);
 
 		 }
-		
-	 void deleteNode(int num, bstNode<t> * &nodePtr)
+	void deleteNode(int num, bstNode<t> * &nodePtr)
 		 {
 		 if (nodePtr && num < nodePtr->data)
 			 deleteNode(num, nodePtr->left);
@@ -106,13 +107,12 @@ public:
 		 else
 			 makeDeletion(nodePtr);
 		 }
-
-	 void makeDeletion(bstNode<t>*& nodePtr) {
+	void makeDeletion(bstNode<t> *& nodePtr) {
 		 // Define a temporary pointer to use in reattaching
 		 // the left subtree.
 		 bstNode<t>* tempNodePtr = nullptr;
-		
-		
+		 
+
 
 		 if (nodePtr == nullptr)
 			 cout << "Cannot delete empty node.\n";
@@ -131,26 +131,131 @@ public:
 		 else {
 			 // Move one node to the right.
 			 tempNodePtr = nodePtr->right;
-			 // Go to the end left node.
-			 while (tempNodePtr->left)
+			 // Go to the end left node.                           
+			 while (tempNodePtr->left)                            
 				 tempNodePtr = tempNodePtr->left;
 			 // Reattach the left subtree.
 			 tempNodePtr->left = nodePtr->left;
+			 
 			 tempNodePtr = nodePtr;
 			 // Reattach the right subtree.
 			 nodePtr = nodePtr->right;
 			 delete tempNodePtr;
+			 tempNodePtr = nullptr;
 		 }
 	 }
 
+
+
+	void deletion(t val) {
+		bstNode<t>* tempNodePtr = nullptr;
+		bstNode<t>* nodePtr = root;
+		bstNode<t>* parent = nullptr;
+
+		// Search for the node to be deleted and keep track of its parent
+		while (nodePtr) {
+			if (nodePtr->data == val) {
+				break;
+			}
+			else {
+				parent = nodePtr;
+				if (nodePtr->data > val) {
+					nodePtr = nodePtr->left;
+				}
+				else {
+					nodePtr = nodePtr->right;
+				}
+			}
+		}
+
+		if (nodePtr == nullptr) {
+			cout << "Cannot delete empty node.\n";
+			return;
+		}
+
+		// Case 1: Node has no children
+		if (nodePtr->left == nullptr && nodePtr->right == nullptr) {
+			if (parent == nullptr) {
+				root = nullptr;
+			}
+			else if (parent->left == nodePtr) {
+				parent->left = nullptr;
+			}
+			else {
+				parent->right = nullptr;
+			}
+			delete nodePtr;
+		}
+		// Case 2: Node has only a left child
+		else if (nodePtr->left != nullptr && nodePtr->right == nullptr) {
+			tempNodePtr = nodePtr->left;
+			if (parent == nullptr) {
+				root = tempNodePtr;
+			}
+			else if (parent->left == nodePtr) {
+				parent->left = tempNodePtr;
+			}
+			else {
+				parent->right = tempNodePtr;
+			}
+			delete nodePtr;
+		}
+		// Case 3: Node has only a right child
+		else if (nodePtr->left == nullptr && nodePtr->right != nullptr) {
+			tempNodePtr = nodePtr->right;
+			if (parent == nullptr) {
+				root = tempNodePtr;
+			}
+			else if (parent->left == nodePtr) {
+				parent->left = tempNodePtr;
+			}
+			else {
+				parent->right = tempNodePtr;
+			}
+			delete nodePtr;
+		}
+
+		// Case 3: Node has both left and right children
+		else {
+			bstNode<t>* successorParent = nodePtr;
+			bstNode<t>* successor = nodePtr->right;
+
+			while (successor->left != nullptr) {
+				successorParent = successor;
+				successor = successor->left;
+			}
+
+			if (successorParent != nodePtr) {
+				successorParent->left = successor->right;
+			}
+			else {
+				successorParent->right = successor->right;
+			}
+
+			nodePtr->data = successor->data;
+			delete successor;
+		}
+	}
+
+	
+
 };
+
+void point(int* a) {
+	cout << a;
+	*a = 4;
+	a = nullptr;
+}
 
 int main() {
 
 
+	
+
 	BST<int> tree;
 	tree.insert(20);
 	tree.insert(22);
+	
 	tree.insert(65);
 	tree.insert(60);
 	tree.insert(25);
@@ -159,10 +264,12 @@ int main() {
 	tree.insert(11);
 	tree.insert(9);
 	tree.insert(70);
+	tree.insert(68);
 	tree.insert(46);
-	tree.remove(20);
+
+	tree.deletion(20);
 	tree.inOrderTraversal(tree.getRoot());
-	
+
 	
 
 }
